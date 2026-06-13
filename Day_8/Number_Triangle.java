266f96f3c9e90c4c1d78dd8bd2a86aa294bb56f2
SHUBHAM <modanwal2509@gmail.com>
2026-06-13 17:17:49 +0530

    Add files via upload

---
+++ Day_8/Number_Triangle.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Number_Triangle {
+  public static void main(String[] args) {
+
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Enter the number of rows : ");
+    int n = input.nextInt();
+
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= i; j++) {
+        System.out.print(j + " ");
+      }
+      System.out.println();
+    }
+  } 
+}
