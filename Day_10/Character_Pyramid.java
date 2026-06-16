63043d4e6945877944161fa188a6a8eadeb24f92
SHUBHAM <modanwal2509@gmail.com>
2026-06-16 11:10:59 +0530

    Add files via upload

---
+++ Day_10/Character_Pyramid.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Character_Pyramid {
+  public static void main(String[] args) {
+
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Enter the number of rows : ");
+    int n = input.nextInt();
+
+    for (int i = 1; i <= n; i++) {
+
+      for (int j = 1; j <= n - i; j++) {
+        System.out.print(" ");
+      }
+      for (int j = 1; j <= i; j++) {
+        System.out.print((char)(j + 64));
+      }
+      for (int j = i-1; j >= 1; j--) {
+        System.out.print((char)(j + 64));
+      }
+      System.out.println();
+    }
+  }
+}
